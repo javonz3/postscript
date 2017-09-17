eeb9af52a7cb63bec597ab3347a799cbd1a902b7
javonz3 <javonz3@yahoo.com>
2017-09-17 02:43:48 -0400

    Delete postscript.sh

--- postscript.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#Proxy Script
-
-
-sudo apt-get update && echo y | sudo apt-get install expect && echo y | sudo apt-get install git && git clone https://github.com/javonz3/squidproxy && cd squidproxy && ./script.py xclusivecops cook 33154
